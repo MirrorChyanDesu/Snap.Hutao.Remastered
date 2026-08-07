00c8987c505035bec3dd02f473f4e852131f1a68
nahida-mouo <nahida-mouo@outlook.com>
2026-08-07 22:49:32 +0800

    整合Snap.Hutao.FileUnlocker

---
+++ src/Snap.Hutao.Remastered/Snap.Hutao.Remastered/Service/FileUnlocker/FileUnlockerErrorResponse.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Snap Hutao RP. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Text.Json.Serialization;
+
+namespace Snap.Hutao.Remastered.Service.FileUnlocker;
+
+public sealed class FileUnlockerErrorResponse
+{
+    [JsonPropertyName("schema")]
+    public string Schema { get; set; } = default!;
+
+    [JsonPropertyName("operation")]
+    public string Operation { get; set; } = default!;
+
+    [JsonPropertyName("errorCode")]
+    public uint ErrorCode { get; set; }
+
+    [JsonPropertyName("errorMessage")]
+    public string ErrorMessage { get; set; } = default!;
+}
